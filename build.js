1ac655b2cc9c0d88afb1d1a1d7c6154165b2089e
alok yadav <alokyadav83956@gmail.com>
2026-01-17 15:24:24 +0530

    Add logging to build.js for debugging

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('Starting to copy public folder to .next');
+
 const src = 'public';
 const dest = '.next';
 
@@ -9,5 +11,9 @@ if (fs.existsSync(src)) {
     const srcPath = path.join(src, item);
     const destPath = path.join(dest, item);
     fs.cpSync(srcPath, destPath, { recursive: true, force: true });
+    console.log(`Copied ${item} to .next`);
   });
+  console.log('Finished copying public folder');
+} else {
+  console.log('Public folder not found');
 }
